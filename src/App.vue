28290c25dd4501b0ee78f78c731260f3c373a97f
dani <dhannizd2@gmail.com>
2025-05-07 11:20:17 +0700

    router

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,17 @@
 <script setup></script>
 
 <template>
-  <h1>Expense Tracker</h1>
+  <div class="app">
+    <h1>Expense Tracker</h1>
+    <router-view></router-view>
+    <!-- Halaman CRUD akan muncul di sini -->
+  </div>
 </template>
+
+<style scoped>
+.app {
+  max-width: 800px;
+  margin: 0 auto;
+  padding: 20px;
+}
+</style>
